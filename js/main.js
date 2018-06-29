1083bde5eb140bdd541dad3f0dc604ad0005935a
Katie Jones <kjones@lookbookhq.com>
2018-06-29 11:21:23 -0400

    some changes

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@
 // document.getElementById('navexpand').addEventListener('click', function() {
 //   navigation.classList.toggle('hide');
 // })
+
+var hamburger = $('.hamburger');
+var nav = $('.navbar');
+
+
+  hamburger.click(() => {
+    nav.slideToggle();
+  });
+
+$('.recipeImg').fluidbox();
+
+var scroll = new SmoothScroll('a[href*="#"]');
